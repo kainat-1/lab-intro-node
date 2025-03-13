988a9af37dff0805b8b3ea38cf32cac99236a88b
kainat <Kainatn002@gmail.com>
2025-03-13 16:31:13 +0100

    solved

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.item = [];
+    this.length = this.item.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.item.push(item);
+    this.item.sort((a, b) => a - b);
+    this.length = this.item.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else return this.item[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return this.item[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    } else return this.item[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.item.reduce((acc, curr) => acc + curr, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
+
+const list = new SortedList();
+
+list.add(3);
+list.add(1);
+list.add(7);
+console.log(list.item);
+console.log(list.length);
+console.log(list.get(1));
+//console.log(list.get(5));
+console.log(list.max());
+console.log(list.sum());
+console.log(list.avg());
